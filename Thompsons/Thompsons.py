abff23834ad872f0c88c1ffe52720f00e4b4254f
Uzzy97 <g00345816@gmit.ie>
2019-03-28 01:18:02 +0000

    Started Thompson's Construction

---
+++ Thompsons/Thompsons.py
@@ -0,0 +1,14 @@
+# Thompson's Construction
+# Usman Sattar
+# G00345816
+
+#Class called state
+class state:
+    # Variables with no values yet
+    label = None
+    edge1 = None
+    edge2 = None
+
+class nfa:
+    initial = None
+    accept = None
